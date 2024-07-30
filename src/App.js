565e157cb7fdd176c1bc65edd2a71d926120472c
Adib Moghimi <adib1991moghimi@gmail.com>
2024-07-30 13:37:02 +0330

    Static mode with minimum styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react/jsx-runtime";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RepaymentApp />
     </div>
   );
 }
 
-export default App;
+function RepaymentApp() {
+  return (
+    <main className="repaymentApp">
+      <MortgageCalculator />
+      <Results />
+    </main>
+  );
+}
+
+function Button({ children }) {
+  return <button>{children}</button>;
+}
+
+function Input({ children, htmlFor, id, type, placeholder }) {
+  return (
+    <>
+      <label htmlFor={htmlFor}>{children}</label>
+      <input type={type} id={id} placeholder={placeholder} />
+    </>
+  );
+}
+
+function InputRadio({ children, htmlFor, id, type, name }) {
+  return (
+    <div>
+      <input type={type} id={id} name={name} />
+      <label htmlFor={htmlFor}>{children}</label>
+    </div>
+  );
+}
+
+function MortgageCalculator() {
+  return (
+    <form className="mortgageCalculator">
+      <h2 className="title">Mortgage Calculator</h2>
+      <span className="clear">Clear All</span>
+
+      <Input
+        htmlFor={"Mortgage-Amount"}
+        type={"text"}
+        id={"Mortgage-Amount"}
+        placeholder={"£"}
+      >
+        {"Mortgage Amount"}
+      </Input>
+
+      <Input
+        htmlFor={"Mortgage-Term"}
+        type={"text"}
+        id={"Mortgage-Term"}
+        placeholder={"Years"}
+      >
+        {"Mortgage Term"}
+      </Input>
+
+      <Input
+        htmlFor={"Interest-Rate"}
+        type={"text"}
+        id={"Interest-Rate"}
+        placeholder={"%"}
+      >
+        {"Interest Rate"}
+      </Input>
+
+      <span>Mortgage Type</span>
+
+      <InputRadio
+        htmlFor={"Repayment"}
+        type={"radio"}
+        id={"Repayment"}
+        name={"Mortgage-Type"}
+      >
+        {"Repayment"}
+      </InputRadio>
+
+      <InputRadio
+        htmlFor={"Interest-Only"}
+        type={"radio"}
+        id={"Interest-Only"}
+        name={"Mortgage-Type"}
+      >
+        {"Interest Only"}
+      </InputRadio>
+
+      <Button>
+        <img src="./icon-calculator.svg" alt="icon-calculator" />
+        {"Calculate Repayments"}
+      </Button>
+    </form>
+  );
+}
+
+function Results() {
+  return (
+    <>
+      <HiddenResult />
+      <VisibleResult />
+    </>
+  );
+}
+
+function HiddenResult() {
+  return (
+    <div className="HiddenResult">
+      <img
+        className="illustration"
+        src="./illustration-empty.svg"
+        alt="illustration-empty"
+      />
+      <h2 className="title">Results shown here</h2>
+      <p className="text">
+        Complete the form and click “calculate repayments” to see what your
+        monthly repayments would be.
+      </p>
+    </div>
+  );
+}
+
+function VisibleResult() {
+  return (
+    <div className="VisibleResult">
+      <h3 className="title">Your results</h3>
+      <p className="text">
+        Your results are shown below based on the information you provided. To
+        adjust the results, edit the form and click “calculate repayments”
+        again.
+      </p>
+      <section className="wrap-monthly-repayments">
+        <span className="title">Your monthly repayments</span>
+        <span className="monthly-repayments">X</span>
+        <span className="title">Total you'll repay over the term</span>
+        <span className="total-repayments">Y</span>
+      </section>
+    </div>
+  );
+}
